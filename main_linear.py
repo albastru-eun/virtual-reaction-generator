9aa6f260591f1d6366f58a48ab2cb8a52a742dd0
albastru-eun <khagrace@gm.gist.ac.kr>
2026-05-20 14:48:32 +0900

    Add files via upload

--- main_linear.py
+++ main_linear.py
@@ -166,7 +166,8 @@ while module != 0:
     elif module == 4:
         print("Graph Mixer Selected")
         n_iter = get_size("n_iter?: ")
-        aggressive = get_size("aggressive? (1 for strain filtering, 2 for non-filter): ")
+        aggressive = get_size("Mode? (1 for strain filtering, 2 for non-filter): ")
+        aggressive -= 1
         GM.graph_mixer_linear_mod(reaction_groups, reaction_groups_val, n_iter, aggressive)
         num_tries += 1
         continue
